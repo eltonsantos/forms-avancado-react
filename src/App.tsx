25afcfe9e12893f31d47d50f66647df9b5d200e3
Elton Santos <eltonaxl@hotmail.com>
2023-04-16 12:24:40 -0300

    Transformation using zod

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,23 @@ import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 
 const createUserFormSchema = z.object({
+  name: z
+    .string()
+    .nonempty("O nome é obrigatório")
+    .transform((name) => {
+      return name
+        .trim()
+        .split(" ")
+        .map((word) => {
+          return word[0].toLocaleUpperCase().concat(word.substring(1));
+        })
+        .join(" ");
+    }),
   email: z
     .string()
     .nonempty("O email é obrigatório")
-    .email("Formato de email inválido"),
+    .email("Formato de email inválido")
+    .toLowerCase(),
   password: z.string().min(6, "A senha precisa de no mínimo 6 caracteres"),
 });
 
@@ -36,7 +49,17 @@ export function App() {
         className="flex flex-col gap-4 w-full max-w-xs"
       >
         <div className="flex flex-col gap-1">
-          <label htmlFor="">Email</label>
+          <label htmlFor="name">Nome</label>
+          <input
+            type="text"
+            className="border border-zinc-600 shadow-sm rounded h-10 px-3 bg-zinc-900 text-white"
+            {...register("name")}
+          />
+          {errors.name && <span>{errors.name.message}</span>}
+        </div>
+
+        <div className="flex flex-col gap-1">
+          <label htmlFor="email">Email</label>
           <input
             type="email"
             className="border border-zinc-600 shadow-sm rounded h-10 px-3 bg-zinc-900 text-white"
@@ -46,7 +69,7 @@ export function App() {
         </div>
 
         <div className="flex flex-col gap-1">
-          <label htmlFor="">Senha</label>
+          <label htmlFor="password">Senha</label>
           <input
             type="password"
             className="border border-zinc-600 shadow-sm rounded h-10 px-3 bg-zinc-900 text-white"
